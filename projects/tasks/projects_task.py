985ec0a47d34d10e7b0e00982835dac53e027159
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2023-09-09 08:53:43 -0500

    modelos para project y registro de falla

--- projects/tasks/projects_task.py
+++ projects/tasks/projects_task.py
@@ -1,5 +1,7 @@
 import datetime
 from celery import Celery
+from ..models.declarative_base import Base, engine, session
+from ..models.models import *
 import random
 
 celery_app = Celery('health', broker='redis://localhost:6379/0')
@@ -19,6 +21,10 @@ def check(message):
 
     if random_number <= 0.9:
         celery_app.send_task('tasks.health_response', args=['projects', message], queue='monitor')
-        
+    else:
+        failure = ServiceRegistration(requestId = message, timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), serviceName='projects')
+        session.add(failure)
+        session.commit()
+        session.close()   
 
    
